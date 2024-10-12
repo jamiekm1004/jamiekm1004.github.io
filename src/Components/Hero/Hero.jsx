bcc8e0cf67bc00843581fc9822c9f32d0f01ce43
jamiekim <jamiekm1004@gmail.com>
2024-10-11 20:20:21 -0700

    Updated home

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return(
     <div id='home' className='hero'>
       <img src={profile_img} alt="" />
-      <h1><span>I'm Jamie Kim,</span> an ML Engineer based in the U.S.</h1>
+      <h1><span>I'm Jamie Kim</span></h1>
       <p>I'm graduating from UC Berkeley in Fall 2024 with a major in Statistics. I'm passionate about machine learning and am looking for full-time opportunities as an ML Engineer.</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
